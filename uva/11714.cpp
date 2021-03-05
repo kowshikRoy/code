4ceea63d0d8ab3e46857dc1e547b9a8380205330
Repon Kumar Roy <repon.roy@hotmail.com>
2021-03-06 06:59:24 +0800

    add uva/11714

---
+++ uva/11714.cpp
@@ -0,0 +1,20 @@
+/**
+ * Author: Repon Kumar Roy
+ * Date: 2021-03-06
+ * Task: 11714
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long n, ans = 0;
+    while (scanf("%lld", &n) == 1) {
+        ans = 1;
+        while (n > 2) {
+            ans += (n / 2) + 1;
+            n = (n + 1) / 2;
+        }
+        printf("%lld\n", ans);
+    }
+}
