abdfe5edcc0224378228db2d91114b996fecf1bd
Repon Kumar Roy <repon.roy@hotmail.com>
2021-03-23 08:38:03 +0800

    feat(kickstart): Add 2021-kickstart-round-A

---
+++ kickstart/2021-round-a/A.cpp
@@ -0,0 +1,64 @@
+/**
+ * Author: Repon Kumar Roy
+ * Date: 2021-03-21
+ * Task: A 
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define LL                      long long
+#define Ulong                   unsigned long long
+#define REP(i,n)                for(long long i = 0; i < n ; i++ )
+#define mp                      make_pair
+#define pb                      push_back
+#define all(x)                  (x).begin(),(x).end()
+#define PI                      acos(-1.0)
+#define EPS                     1e-14
+#define F                       first
+#define S                       second
+#define in(x)                   input(x)
+#define in2(x,y)                in(x),in(y)
+#define in3(x,y,z)              in(x),in2(y,z)
+#define lc                      ((n)<<1)
+#define rc                      ((n)<<1|1)
+#define db(x)                   cout << #x << " -> " << x << endl
+
+template<class T> inline void input(T &x) {
+  register char c = getchar();x = 0;
+  int neg = 0;
+  for(; ((c<48 || c>57) && c != '-'); c = getchar());
+  if(c=='-'){neg = 1;c = getchar();}
+  for(; c>47 && c<58 ; c = getchar()){x = (x<<1) + (x<<3) + c - 48;}
+  if(neg) x = -x;
+}
+inline long long bigmod(long long p,long long e,long long M){
+  long long ret = 1;
+  for(; e > 0; e >>= 1){
+    if(e & 1) ret = (ret * p) % M;
+    p = (p * p) % M;
+  } return ret;
+}
+template <class T> inline T gcd(T a,T b){if(b==0)return a;return gcd(b,a%b);}
+template <class T> inline T modinverse(T a,T M){return bigmod(a,M-2,M);}
+ 
+/********************************************************/ 
+
+int main() {
+    int t,cs = 0;
+    cin >> t;
+    while(t--) {
+        int n,k;
+        cin >> n >> k;
+        char s[n + 2];
+        cin >> (s+1);
+        int ans = 0;
+        for(int i = 1; i <= n/2; i ++) {
+            if(s[i] != s[n - i + 1]) ans ++;
+        }
+
+        printf("Case #%d: ", ++cs);
+        cout << abs(ans - k) << endl;
+    }
+    return 0;
+}
---
+++ kickstart/2021-round-a/C.cpp
@@ -0,0 +1,215 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using db = double; 
+using str = string; // yay python!
+ 
+using pi = pair<int,int>;
+using pl = pair<ll,ll>;
+using pd = pair<db,db>;
+ 
+using vi = vector<int>;
+using vb = vector<bool>;
+using vl = vector<ll>;
+using vd = vector<db>; 
+using vs = vector<str>;
+using vpi = vector<pi>;
+using vpl = vector<pl>; 
+using vpd = vector<pd>;
+ 
+#define tcT template<class T
+#define tcTU tcT, class U
+// ^ lol this makes everything look weird but I'll try it
+tcT> using V = vector<T>; 
+tcT, size_t SZ> using AR = array<T,SZ>; 
+tcT> using PR = pair<T,T>;
+ 
+// pairs
+#define mp make_pair
+#define f first
+#define s second
+ 
+#define sz(x) (int)(x).size()
+ 
+// loops
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define F0R(i,a) FOR(i,0,a)
+#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i,a) ROF(i,0,a)
+#define trav(a,x) for (auto& a: x)
+ 
+ // INPUT
+#define tcTUU tcT, class ...U
+tcT> void re(complex<T>& c);
+tcTU> void re(pair<T,U>& p);
+tcT> void re(vector<T>& v);
+tcT, size_t SZ> void re(AR<T,SZ>& a);
+ 
+tcT> void re(T& x) { cin >> x; }
+void re(db& d) { str t; re(t); d = stod(t); }
+void re(ld& d) { str t; re(t); d = stold(t); }
+tcTUU> void re(T& t, U&... u) { re(t); re(u...); }
+ 
+tcT> void re(complex<T>& c) { T a,b; re(a,b); c = {a,b}; }
+tcTU> void re(pair<T,U>& p) { re(p.f,p.s); }
+tcT> void re(vector<T>& x) { trav(a,x) re(a); }
+tcT, size_t SZ> void re(AR<T,SZ>& x) { trav(a,x) re(a); }
+tcT> void rv(int& n, vector<T>& x) { re(n); x.rsz(n); trav(a,x) re(a); }
+ 
+// TO_STRING
+#define ts to_string
+str ts(char c) { return str(1,c); }
+str ts(const char* s) { return (str)s; }
+str ts(str s) { return s; }
+str ts(bool b) { 
+    #ifdef LOCAL
+        return b ? "true" : "false"; 
+    #else 
+        return ts((int)b);
+    #endif
+}
+tcT> str ts(complex<T> c) { 
+    stringstream ss; ss << c; return ss.str(); }
+str ts(vector<bool> v) {
+    str res = "{"; F0R(i,sz(v)) res += char('0'+v[i]);
+    res += "}"; return res; }
+template<size_t SZ> str ts(bitset<SZ> b) {
+    str res = ""; F0R(i,SZ) res += char('0'+b[i]);
+    return res; }
+tcTU> str ts(pair<T,U> p);
+tcT> str ts(T v) { // containers with begin(), end()
+    #ifdef LOCAL
+        bool fst = 1; str res = "{";
+        for (const auto& x: v) {
+            if (!fst) res += ", ";
+            fst = 0; res += ts(x);
+        }
+        res += "}"; return res;
+    #else
+        bool fst = 1; str res = "";
+        for (const auto& x: v) {
+            if (!fst) res += " ";
+            fst = 0; res += ts(x);
+        }
+        return res;
+ 
+    #endif
+}
+tcTU> str ts(pair<T,U> p) {
+    #ifdef LOCAL
+        return "("+ts(p.f)+", "+ts(p.s)+")"; 
+    #else
+        return ts(p.f)+" "+ts(p.s);
+    #endif
+}
+ 
+// OUTPUT
+tcT> void pr(T x) { cout << ts(x); }
+tcTUU> void pr(const T& t, const U&... u) { 
+    pr(t); pr(u...); }
+void ps() { pr("\n"); } // print w/ spaces
+tcTUU> void ps(const T& t, const U&... u) { 
+    pr(t); if (sizeof...(u)) pr(" "); ps(u...); }
+ 
+ // DEBUG
+void DBG() { cerr << "]" << endl; }
+tcTUU> void DBG(const T& t, const U&... u) {
+    cerr << ts(t); if (sizeof...(u)) cerr << ", ";
+    DBG(u...); }
+#ifdef LOCAL // compile with -DLOCAL, chk -> fake assert
+    #define dbg(...) cerr << "Line(" << __LINE__ << ") -> [" << #__VA_ARGS__ << "]: [", DBG(__VA_ARGS__)
+    #define chk(...) if (!(__VA_ARGS__)) cerr << "Line(" << __LINE__ << ") -> function(" \
+         << __FUNCTION__  << ") -> CHK FAILED: (" << #__VA_ARGS__ << ")" << "\n", exit(0);
+#else
+    #define dbg(...) 0
+    #define chk(...) 0
+#endif
+
+/**
+ * Author: Repon Kumar Roy
+ * Date: 2021-03-21
+ * Task: C 
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define LL                      long long
+#define Ulong                   unsigned long long
+#define REP(i,n)                for(long long i = 0; i < n ; i++ )
+#define mp                      make_pair
+#define pb                      push_back
+#define all(x)                  (x).begin(),(x).end()
+#define PI                      acos(-1.0)
+#define EPS                     1e-14
+#define F                       first
+#define S                       second
+#define in(x)                   input(x)
+#define in2(x,y)                in(x),in(y)
+#define in3(x,y,z)              in(x),in2(y,z)
+#define lc                      ((n)<<1)
+#define rc                      ((n)<<1|1)
+#define db(x)                   cout << #x << " -> " << x << endl
+
+template<class T> inline void input(T &x) {
+  register char c = getchar();x = 0;
+  int neg = 0;
+  for(; ((c<48 || c>57) && c != '-'); c = getchar());
+  if(c=='-'){neg = 1;c = getchar();}
+  for(; c>47 && c<58 ; c = getchar()){x = (x<<1) + (x<<3) + c - 48;}
+  if(neg) x = -x;
+}
+inline long long bigmod(long long p,long long e,long long M){
+  long long ret = 1;
+  for(; e > 0; e >>= 1){
+    if(e & 1) ret = (ret * p) % M;
+    p = (p * p) % M;
+  } return ret;
+}
+template <class T> inline T gcd(T a,T b){if(b==0)return a;return gcd(b,a%b);}
+template <class T> inline T modinverse(T a,T M){return bigmod(a,M-2,M);}
+ 
+/********************************************************/ 
+int dir[][2] = {{0,1},{0,-1},{1,0},{-1,0}};
+
+int A[404][404];
+bool vis[404][404];
+int main() {
+    int t,cs= 0;
+    cin >> t;
+    while(t--) {
+        int r,c;
+        cin >> r >> c;
+        set<pair<int,pair<int,int>>> s;
+        for(int i = 0; i < r; i ++) {
+            for(int j = 0; j < c; j ++) {
+                cin >> A[i][j];
+                vis[i][j] = 0;
+                s.insert({A[i][j], {i,j}});
+            }
+        }
+        long long ans = 0;
+        while(s.size()) {
+            /* dbg(s); */
+            auto p = *s.rbegin(); s.erase(p); vis[p.second.first][p.second.second] = 1;
+            for(int i = 0; i < 4; i ++) {
+                int x = p.second.first + dir[i][0];
+                int y = p.second.second + dir[i][1];
+                if(vis[x][y] == 0 && x >= 0 && x < r && y >= 0 && y < c) {
+                    int add = max(0, p.first -1 - A[x][y]);
+                    ans += add;
+                    s.erase({A[x][y], {x,y}});
+                    A[x][y] += add;
+                    s.insert({A[x][y], {x,y}});
+                }
+            }
+        }
+
+        printf("Case #%d: %lld\n", ++cs, ans);
+
+    }
+    return 0;
+}
