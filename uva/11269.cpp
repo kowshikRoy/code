8a4ee0e2e52f8a2e4a26cf3a017f26d58663f9ec
Repon Kumar Roy <repon.roy@hotmail.com>
2021-04-15 16:46:20 +0800

    feat(uva): Add uva/11269.cpp solution

---
+++ uva/11269.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+ 
+void dbg_out() { cerr << "]" << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << H; if(sizeof...(T)) cerr << ", "; dbg_out(T...); }
+#ifdef LOCAL
+#define dbg(...) cerr << "Line(" << __LINE__ << ") -> [" << #__VA_ARGS__ << "]: [", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+/**
+ * Author: Repon Kumar Roy
+ * Date: 2021-04-13
+ * Task: 11269 
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll        long long
+#define REP(i, n) for (int i = 0; i < (n); i++)
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    while(cin>>n) {
+        vector<int> a(n), b(n),id(n);
+        REP(i,n) cin >>a[i];
+        REP(i,n) cin >> b[i];
+
+        cout<<ans<<endl;
+
+
+    }
+    return 0;
+}
+
