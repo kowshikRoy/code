16b44294001cb4719054de4b7c779586b5672ec0
Repon Kumar Roy <repon.roy@hotmail.com>
2021-04-15 16:49:58 +0800

    feat(cses): Add cses/1188.cpp solution

---
+++ cses/1188.cpp
@@ -0,0 +1,30 @@
+/**
+ * Author: Repon Kumar Roy
+ * Date: 2021-03-28
+ * Task: 1188 
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll        long long
+#define REP(i, n) for (int i = 0; i < (n); i++)
+
+void solve() {
+    string s;
+    cin>>s;
+    int n = (int)s.size();
+
+
+    int m;
+    cin>>m;
+    for(int i = 0;
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    return 0;
+}
+
