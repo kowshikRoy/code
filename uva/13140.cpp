bbbe018dc4377f7b00886bca68829ede9d437100
Repon Kumar Roy <repon.roy@hotmail.com>
2021-04-15 16:46:20 +0800

    feat(uva): Add uva/13140.cpp solution

---
+++ uva/13140.cpp
@@ -0,0 +1,19 @@
+/**
+ * Author: Repon Kumar Roy
+ * Date: 2021-04-15
+ * Task: 13140
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll        long long
+#define REP(i, n) for (int i = 0; i < (n); i++)
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    for (int i = 9999; i <= 10005; i++) { cout << i << " " << i * i << endl; }
+    return 0;
+}
