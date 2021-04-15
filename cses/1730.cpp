684fa8c97278aede00fe6dc5b8ec686800ebfff2
Repon Kumar Roy <repon.roy@hotmail.com>
2021-04-15 16:49:59 +0800

    feat(cses): Add cses/1730.cpp solution

---
+++ cses/1730.cpp
@@ -0,0 +1,28 @@
+/**
+ * Author: Repon Kumar Roy
+ * Date: 2021-04-02
+ * Task: 1730
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll        long long
+#define REP(i, n) for (int i = 0; i < (n); i++)
+
+void solve() {
+    int n;
+    cin >> n;
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--) { solve(); }
+    return 0;
+}
