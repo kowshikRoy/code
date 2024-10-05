51bbd6e59946990d9446eaf56f6e05158e25723d
Repon Kumar Roy <repon.roy@hotmail.com>
2024-10-05 04:40:22 +0200

    Add leetcode/12

---
+++ leetcode/integer_to_roman.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_INTEGER_TO_ROMAN_H_
+#define MAIN_INTEGER_TO_ROMAN_H_
+
+#include <string>
+class Solution{
+    public:
+    std::string intToRoman(int num);
+};
+
+#endif
---
+++ leetcode/integer_to_roman_test.cc
@@ -0,0 +1,27 @@
+#include "gtest/gtest.h"
+#include "integer_to_roman.h"
+
+TEST(IntegerToRomanTest, Test1) {
+    Solution solution;
+    ASSERT_EQ(solution.intToRoman(3), "III");
+}
+
+TEST(IntegerToRomanTest, Test2) {
+    Solution solution;
+    ASSERT_EQ(solution.intToRoman(4), "IV");
+}
+
+TEST(IntegerToRomanTest, Test3) {
+    Solution solution;
+    ASSERT_EQ(solution.intToRoman(9), "IX");
+}
+
+TEST(IntegerToRomanTest, Test4) {
+    Solution solution;
+    ASSERT_EQ(solution.intToRoman(58), "LVIII");
+}
+
+TEST(IntegerToRomanTest, Test5) {
+    Solution solution;
+    ASSERT_EQ(solution.intToRoman(1994), "MCMXCIV");
+}
